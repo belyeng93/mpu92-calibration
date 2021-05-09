76d21d0e052f14ee8e139c635de2f1f6af10e2a6
belyeng93 <belyeng93@gmail.com>
2021-05-09 20:09:09 +0200

    Added arduino sketches for serial data

---
+++ arduino_sketches/mpu9250Acc/mpu9250Acc.ino
@@ -0,0 +1,28 @@
+#include "MPU9250.h"
+MPU9250 mpu; // You can also use MPU9255 as is
+
+void setup() {
+    Serial.begin(115200);
+    Wire.begin();
+    delay(2000);
+
+     if (!mpu.setup(0x68)) {  // change to your own address
+        while (1) {
+            Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+            delay(5000);
+        }
+        Serial.println("MPU connection OK!");
+    }
+}
+
+void loop() {
+    if (mpu.update()) {
+        Serial.print(mpu.getAccX());
+        Serial.print(", ");
+        Serial.print(mpu.getAccY());
+        Serial.print(", ");
+        Serial.print(mpu.getAccZ());
+        Serial.println();
+        delay(1000);
+    }
+}
